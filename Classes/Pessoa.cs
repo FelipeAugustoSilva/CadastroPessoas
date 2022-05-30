9b54028336be567249053157e1059c3dcb351f00
Felipe Augusto da Silva <felipeaugustodasilva23@gmail.com>
2022-05-30 05:22:57 -0300

    Cadastro Pessoas

---
+++ Classes/Pessoa.cs
@@ -0,0 +1,16 @@
+using CadastroPessoas.Interfaces;
+
+namespace CadastroPessoas.Classes
+{
+    public abstract class Pessoa : IPessoa
+    {
+        public string? Name { get; set; }
+
+        public float Rendimento { get; set; }
+
+        public string? Endereco { get; set; }
+
+        public abstract float PagarImposto(float rendimento);
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+using CadastroPessoas.Classes;
+
+PessoaFisica novaPf = new PessoaFisica();
+
+novaPf.Name = "Felipe";
+
+Console.WriteLine(novaPf.Name);
+
+Console.WriteLine($"Nome: {novaPf.Name} Nome: {novaPf.Name}");
+Console.WriteLine("Nome: " + novaPf.Name + " Nome:" + novaPf.Name);
