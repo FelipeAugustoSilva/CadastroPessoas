989de1ab8ff23a5699f9610561d5914de53e955e
Felipe Augusto da Silva <felipeaugustodasilva23@gmail.com>
2022-06-11 03:20:42 -0300

    Encontro Remoto 03/06

--- Classes/PessoaJuridica.cs
+++ Classes/PessoaJuridica.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using CadastroPessoas.Interfaces;
 
 namespace CadastroPessoas.Classes
@@ -7,16 +8,47 @@ namespace CadastroPessoas.Classes
         public string? Cnpj { get; set; }
 
         public string? RazaoSocial { get; set; }
-        
-    
+
+
         public override float PagarImposto(float rendimento)
         {
             throw new NotImplementedException();
         }
 
+
+        //XX.XXX.XXX/0001-XX ----- XXXXXXXX0001XX
         public bool ValidarCnpj(string cnpj)
         {
-            throw new NotImplementedException();
+            bool retornoCnpjValido = Regex.IsMatch(cnpj, @"(^(\d{2}.\d{3}.\d{3}/\{4}-\d{2})|(\d{14})$)");
+
+            //if (Regex.IsMatch(cnpj,  @"(^(\d{2}.\d{3}.\d{3}/\{4}-\d{2})|(\d{14})$)"))
+            if (retornoCnpjValido == true)
+            {
+                if (cnpj.Length == 18)
+                {
+                    string subStringCnpj = cnpj.Substring(11, 4);
+
+                    if (subStringCnpj == "0001")
+                    {
+                        return true;
+                    }
+
+                }
+                else if (cnpj.Length == 14)
+                {
+                    string subStringCnpj = cnpj.Substring(8, 4);
+
+                    if (subStringCnpj == "0001")
+                    {
+                        return true;
+                    }
+
+                }
+            }
+            return false;
+
+
+
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,36 +1,58 @@
 ﻿
 using CadastroPessoas.Classes;
 
-PessoaFisica novaPf = new PessoaFisica();
-PessoaFisica metodosPf = new PessoaFisica();
-Endereco novoEndPf = new Endereco();
+// PessoaFisica novaPf = new PessoaFisica();
+// PessoaFisica metodosPf = new PessoaFisica();
+// Endereco novoEndPf = new Endereco();
 
-novaPf.Name = "Felipe";
-novaPf.dataNasc = new DateTime (2000, 01, 01);
-novaPf.Cpf = "1234567890";
-novaPf.Rendimento = 3500.5f;
+// novaPf.Name = "Felipe";
+// novaPf.dataNasc = new DateTime (2000, 01, 01);
+// novaPf.Cpf = "1234567890";
+// novaPf.Rendimento = 3500.5f;
 
-novoEndPf.logradouro = "Alameda barao de limeira";
-novoEndPf.numero = 539;
-novoEndPf.complemento = "Senai Informatica";
-novoEndPf.endComercial = true;
+// novoEndPf.logradouro = "Alameda barao de limeira";
+// novoEndPf.numero = 539;
+// novoEndPf.complemento = "Senai Informatica";
+// novoEndPf.endComercial = true;
 
-novaPf.Endereco = novoEndPf;
-
-
-Console.WriteLine(@$"
-Nome: {novaPf.Name}
-Endereço: {novaPf.Endereco.logradouro}, {novaPf.Endereco.numero}
-Maior de idade: {metodosPf.ValidarDataNasc(novaPf.dataNasc)}
-
-");
+// novaPf.Endereco = novoEndPf;
 
 
+// Console.WriteLine(@$"
+// Nome: {novaPf.Name}
+// Endereço: {novaPf.Endereco.logradouro}, {novaPf.Endereco.numero}
+// Maior de idade: {metodosPf.ValidarDataNasc(novaPf.dataNasc)}
+// ");
 
 
 //Console.WriteLine(metodosPf.ValidarDataNasc(novaPf.dataNasc));
-
 //Console.WriteLine(novaPf.Name);
-
 //Console.WriteLine($"Nome: {novaPf.Name} Nome: {novaPf.Name}");
 //Console.WriteLine("Nome: " + novaPf.Name + " Nome:" + novaPf.Name);
+
+
+PessoaJuridica novaPj = new PessoaJuridica();
+PessoaJuridica metodosPj = new PessoaJuridica();
+Endereco novoEndPj = new Endereco();
+
+novaPj.Name = "Nome Pj";
+novaPj.RazaoSocial = "Razão Social Pj";
+novaPj.Cnpj = "00000000000100";
+novaPj.Rendimento = 10000.5f;
+
+novoEndPj.logradouro = "alameda barão de limeira";
+novoEndPj.numero = 539;
+novoEndPj.complemento = "Senai Informatica";
+novoEndPj.endComercial = true;
+
+novaPj.Endereco = novoEndPj;
+
+//Console.WriteLine(metodosPj.ValidarCnpj(novaPj.Cnpj));
+Console.WriteLine(@$"
+Nome: {novaPj.Name}
+Razão Social: {novaPj.RazaoSocial}
+CNPJ: {novaPj.Cnpj}, Valido: {metodosPj.ValidarCnpj(novaPj.Cnpj)}
+Endereço: {novaPj.Endereco.logradouro}, N: {novaPj.Endereco.numero}
+Complemento: {novaPj.Endereco.complemento}
+");
+
